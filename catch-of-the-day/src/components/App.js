d8010650ac0da8bac75f63a54a30c113a6e2bdc2
Shan <shan.mi.fanfan@gmail.com>
2020-06-01 09:44:03 +0200

    added Fish.jsx component

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
 import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
 
 class App extends React.Component {
   state = {
@@ -20,13 +21,18 @@ class App extends React.Component {
   };
 
   loadSampleFishes = () => {
-    this.setState({ fishes: sampleFishes })
+    this.setState({ fishes: sampleFishes });
   };
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header tagline="Fresh Seafood Market"></Header>
+          <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map((key) => (
+              <Fish key={key} />
+            ))}
+          </ul>
         </div>
         <Order />
         <Inventory
---
+++ catch-of-the-day/src/components/Fish.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class Fish extends React.Component {
+  render() {
+    return (
+      <div className="Fish">
+        <span role="img" aria-label="fish emoji">
+          🐟
+        </span>{" "}
+      </div>
+    );
+  }
+}
